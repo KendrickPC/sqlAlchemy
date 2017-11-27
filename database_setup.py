e8943c44f52a70510f897a5e767629bcbe40cf85
KendrickPC <kennethpchang@gmail.com>
2017-11-27 18:03:29 +0800

    SQL mapper class written out in psuedocode

--- database_setup.py
+++ database_setup.py
@@ -21,7 +21,8 @@ Base.metadata.create_all(engine)
 
 
 
-# creating a class and table. Note that this should be placed between the configuration code
+# creating a class and table. 
+# Note that this should be placed between the configuration code
 
 class Restaurant(Base): 
 
@@ -31,3 +32,23 @@ __tablename__ = 'restaurant'
 class MenuItem(Base):
 
 __tablename__ = 'menu_item'
+
+# mapper code used to create columns in the database
+# inside Restaurant Class
+
+name = Column(
+String(80), nullable = False)
+
+id = Column (
+Integer, primary_key = True)
+
+# Inside Menu Item Class. Still MAPPER. 
+
+name = Column(String(80), nullable = False)
+id = Column(Integer, primary_key = True)
+course = Column(String(250))
+description = Column(String(250))
+price = Column(String(8))
+restaurant_id = Column (
+Integer, ForeignKey('restaurant.id'))
+restaurant = relationship(Restaurant)
