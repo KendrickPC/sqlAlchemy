10416d1cf23e9041ad3a6855aaf0973fe81a4f0f
KendrickPC <kennethpchang@gmail.com>
2017-11-27 18:51:54 +0800

    database_setup.py working with virtualbox and sqlalchemy

--- database_setup.py
+++ database_setup.py
@@ -4,36 +4,26 @@ from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
-
+ 
 Base = declarative_base()
-
-# creating a class and table.
-# Note that this should be placed between the configuration code
-
-
+ 
 class Restaurant(Base):
-
-	__tablename__ = 'restaurant'
-
-name = Column(String(80), nullable=False)
-
-id = Column(Integer, primary_key=True)
-
-
+    __tablename__ = 'restaurant'
+   
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+ 
 class MenuItem(Base):
-
     __tablename__ = 'menu_item'
 
-name = Column(String(80), nullable=False)
-id = Column(Integer, primary_key=True)
-course = Column(String(250))
-description = Column(String(250))
-price = Column(String(8))
-restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
-restaurant = relationship(Restaurant)
+    name =Column(String(80), nullable = False)
+    id = Column(Integer, primary_key = True)
+    description = Column(String(250))
+    price = Column(String(8))
+    course = Column(String(250))
+    restaurant_id = Column(Integer,ForeignKey('restaurant.id'))
+    restaurant = relationship(Restaurant) 
+ 
 
-
-# insert at the end of file
 engine = create_engine('sqlite:///restaurantmenu.db')
-
 Base.metadata.create_all(engine)
